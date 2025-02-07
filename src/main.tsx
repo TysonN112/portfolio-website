43c4b74c62912b1e9f3f261e12d3c22c6c3bae1f
Tyson Nguyen <tysonnguyen.k@gmail.com>
2025-02-07 13:38:14 -0600

    Updated project with new features/bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,14 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/main.tsx'
+      ]
+    }
+  }
+};
